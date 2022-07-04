2a591bb60d7035a4a5e4634769af089ba9db68a8
Nick Thorpe <nickthorpe71@gmail.com>
2022-07-04 14:26:30 -0500

    add firness function and base algorithm

--- src/gaPlayground/elixirBook/oneMaxProblem.ts
+++ src/gaPlayground/elixirBook/oneMaxProblem.ts
@@ -1,4 +1,5 @@
 import {range} from "../../utils/index";
+import logger from "../../utils/logger";
 
 const populationSize = 100;
 const chromosomeLength = 1000;
@@ -11,5 +12,19 @@ function randomChromosome(length: number): number[] {
     return range(1, length).map(() => Math.random() < 0.5 ? 0 : 1);
 }
 
+function fitness(chromosome: number[]): number {
+  return chromosome.reduce((acc, curr) => acc + curr, 0);
+}
 
-console.log(initialPopulation(populationSize));
+function algorithm(population: number[][]): number[] {
+  const populationClone = population.slice();
+  const best = populationClone.sort(
+    (aChromosome: number[], bChromosome: number[]): number => {
+    return fitness(aChromosome) - fitness(bChromosome);
+  })[0];
+  logger.info(`Current best is ${best}`);
+  return fitness(best) === 1000 ? best : algorithm(populationClone);
+}
+
+
+algorithm(initialPopulation(populationSize));
