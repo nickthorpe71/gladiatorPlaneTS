9b619cebc13fe46d4fb74b5a0158decc238b6569
Nick Thorpe <nickthorpe71@gmail.com>
2022-08-09 07:54:28 -0500

    add functioning order one crossover function

--- src/gaPlayground/Maeve/toolbox/crossover.ts
+++ src/gaPlayground/Maeve/toolbox/crossover.ts
@@ -4,53 +4,57 @@ import { random } from "lodash";
 /**
  * Preserves permutation of genes.
  */
-// function orderOne<T>(
-//     parentA: Chromosome<T>,
-//     parentB: Chromosome<T>
-// ): Chromosome<T>[] {
-//     const limit = parentA.genes.length - 1;
-
-//     // get random range
-//     const [i1, i2] = [random(limit), random(limit)].sort((a, b) => a - b);
-
-//     const parentBContribution = parentB.genes.slice();
-// }
-
-function orderOneTest(
-    parentA: { genes: number[] },
-    parentB: { genes: number[] }
-) {
-    const limit = parentA.genes.length - 1;
-
+function orderOne<T>(
+    parent1: Chromosome<T>,
+    parent2: Chromosome<T>
+): Chromosome<T>[] {
+    const limit = parent1.genes.length - 1;
     // get random range
     const [i1, i2] = [random(limit), random(limit)].sort((a, b) => a - b);
-    const parentBContribution = parentB.genes.slice(i1, i2);
+    const child1: Chromosome<T> = {
+        genes: orderOneCreateChildHelper<T>(parent1, parent2, i1, i2),
+        size: parent1.size,
+        fitness: 0,
+        age: 0,
+    };
+    const child2: Chromosome<T> = {
+        genes: orderOneCreateChildHelper<T>(parent2, parent1, i1, i2),
+        size: parent1.size,
+        fitness: 0,
+        age: 0,
+    };
+    return [child1, child2];
+}
+function orderOneCreateChildHelper<T>(
+    parentA: Chromosome<T>,
+    parentB: Chromosome<T>,
+    i1: number,
+    i2: number
+) {
+    // take a slice of the genes from parentB using predetermined random range
+    const parentBContribution: T[] = parentB.genes.slice(i1, i2);
 
-    // determine which parent 1 genes are not in parent 2 contribution
-    const parentARemainder = parentA.genes.filter(
+    // determine which parentA genes are not in parentB contribution (no duplicates)
+    const parentAContribution: T[] = parentA.genes.filter(
         (gene) => parentBContribution.indexOf(gene) === -1
     );
 
-    const child1Genes: Array<number> = new Array(parentA.genes.length);
+    let parentASelector: number = 0;
+    // create child genes using parentB slice and filling in remaining parentA genes in order
+    const childGenes: Array<T> = Array(parentA.genes.length)
+        .fill(null)
+        .map((_, i) =>
+            i >= i1 && i < i2
+                ? parentBContribution[i - i1]
+                : parentAContribution[parentASelector++]
+        );
 
-    console.log(i1, i2);
-    console.log(parentBContribution);
-    console.log(parentARemainder);
+    return childGenes;
 }
 
 // TODO: once implemented it needs to be reapplied to all old problems
 function singlePoint() {}
 
-// export const crossoverStrategy = {
-//     orderOne,
-// };
-
-const parentA = {
-    genes: [1, 2, 6, 8, 5, 3, 7, 9, 4, 10],
+export const crossoverStrategy = {
+    orderOne,
 };
-
-const parentB = {
-    genes: [5, 9, 3, 7, 1, 6, 8, 2, 10, 4],
-};
-
-console.log(orderOneTest(parentA, parentB));
