d09c2e6cf25a2152c8f232dbdd7e55c87e3d2432
Nick Thorpe <nickthorpe71@gmail.com>
2022-08-10 06:44:52 -0500

    add uniform and multi arithmetic crossover

--- src/gaPlayground/Maeve/toolbox/crossover.ts
+++ src/gaPlayground/Maeve/toolbox/crossover.ts
@@ -2,7 +2,75 @@ import Chromosome from "../types/Chromosome";
 import { random } from "lodash";
 
 /**
+ * Specialized for real-valued chromosomes. May only be used on chromosomes of type number.
+ * @param parent1 Chromosome<T>
+ * @param parent2 Chromosome<T>
+ * @param alpha number
+ * @returns [Chromosome<T>, Chromosome<T>]
+ */
+function wholeArithmetic<T>(
+    parent1: Chromosome<T>,
+    parent2: Chromosome<T>,
+    alpha: number
+) {
+    const child1 = {
+        genes: parent1.genes.map(
+            (gene, i) =>
+                alpha * Number(gene) + (1 - alpha) * Number(parent2.genes[i])
+        ),
+        size: parent1.size,
+        fitness: 0,
+        age: 0,
+    };
+    const child2 = {
+        genes: parent2.genes.map(
+            (gene, i) =>
+                alpha * Number(gene) + (1 - alpha) * Number(parent1.genes[i])
+        ),
+        size: parent1.size,
+        fitness: 0,
+        age: 0,
+    };
+
+    return [child1, child2];
+}
+
+/**
+ * Works best with binary genotypes of small size.
+ * @param parent1 Chromosome<T>
+ * @param parent2 Chromosome<T>
+ * @returns [Chromosome<T>, Chromosome<T>]
+ */
+function uniform<T>(
+    parent1: Chromosome<T>,
+    parent2: Chromosome<T>,
+    rate: number
+): Chromosome<T>[] {
+    const child1: Chromosome<T> = {
+        genes: parent1.genes.map((gene, i) =>
+            random() > rate ? gene : parent2.genes[i]
+        ),
+        size: parent1.size,
+        fitness: 0,
+        age: 0,
+    };
+    const child2: Chromosome<T> = {
+        genes: parent2.genes.map((gene, i) =>
+            random() > rate ? gene : parent1.genes[i]
+        ),
+        size: parent1.size,
+        fitness: 0,
+        age: 0,
+    };
+
+    return [child1, child2];
+}
+
+/**
  * Preserves permutation of genes.
+ * @param parent1 Chromosome<T>
+ * @param parent2 Chromosome<T>
+ * @returns [Chromosome<T>, Chromosome<T>]
  */
 function orderOne<T>(
     parent1: Chromosome<T>,
@@ -52,6 +120,12 @@ function orderOneCreateChildHelper<T>(
     return childGenes;
 }
 
+/**
+ * A simple crossover function for basic problems.
+ * @param parent1 Chromosome<T>
+ * @param parent2 Chromosome<T>
+ * @returns [Chromosome<T>, Chromosome<T>]
+ */
 function singlePoint<T>(parent1: Chromosome<T>, parent2: Chromosome<T>) {
     const crossoverPoint = random(parent1.size);
 
@@ -76,4 +150,9 @@ function singlePoint<T>(parent1: Chromosome<T>, parent2: Chromosome<T>) {
     return [child1, child2];
 }
 
-export const crossoverStrategy = { singlePoint, orderOne };
+export const crossoverStrategy = {
+    singlePoint,
+    orderOne,
+    uniform,
+    wholeArithmetic,
+};
